d7a632ae858f7ab9f6323fcff22221d3610be905
ankToken <ricardocardona.rc@gmail.com>
2017-07-05 01:36:28 -0500

    Hola Mundo en el lenguaje Java.
    Un hola mundo y mensaje de bienvenida a OneDayOneCode. :)

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+package holamundo;//Paquete que contiene al programa.
+
+public class HolaMundo//Nombre de la clase.
+{
+
+	public static void main(String[] args)//Método público estático.
+	{
+		System.out.println("Hola mundo\nBienvenido a OneDayOneCode :)");//Esta setencia imprime una línea en consola.
+	}
+
+}
