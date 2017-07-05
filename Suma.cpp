c896f6fc375cb6d2401570f5f6de4b65bf19dfe2
Marcos-1997 <jigoku_shujin@hotmail.com>
2017-07-05 01:07:56 -0500

    Add files via upload

---
+++ Suma.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+/*
+**
+*** Este programa esta escrito C++ lo que hace es una simple suma
+**
+*/
+
+using namespace std;
+
+class Suma //Se define la clase
+{
+
+	private: 
+
+		int numero_1, numero_2;
+
+	public:
+
+		void obtener_numero_1() //Se obtiene el numero 1
+		{
+			std::cout<<"Numero 1:";
+			std::cin>>this->numero_1;
+		}
+
+		void obtener_numero_2() //Se obtiene el numero 2
+		{
+			std::cout<<"Numero 2:";
+			std::cin>>this->numero_2;
+		}
+
+		int obtener_suma() //Se obtiene el resultado de la suma 
+		{
+			return this->numero_1 + this->numero_2; //Se regresa,el resultado de la suma
+		}
+
+};
+
+
+int main()
+{
+	Suma numero; //Se declara el objeto
+	numero.obtener_numero_1();
+	numero.obtener_numero_2();
+	
+	cout<<"El resultado de la suma es:"<<numero.obtener_suma()<<endl;
+}
