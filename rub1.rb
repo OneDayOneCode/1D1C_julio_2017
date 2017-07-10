5841f3dbc8fd4bed1e31c6c584b08d6152e5e389
Wotan <albertocardona.deb@gmail.com>
2017-07-09 20:42:30 -0500

    variables y operadores en Ruby (muy básico)
    Declaración de variables en Ruby, así como el uso de puts y algunos operadores aritméticos

---
+++ rub1.rb
@@ -0,0 +1,15 @@
+
+puts "Ejemplo básico de variables en Ruby"
+
+
+variableuno = "Ruby1.9.3"
+puts "Hola #{variableuno}"
+
+
+varA = 10
+varB = 5
+
+puts varA+varB
+puts varA**varB, varA%varB
+puts varA*varB, varA/varB
+
