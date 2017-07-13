e870079fdad1ad236205b1fa0ed4c6940e54b577
Marcos-1997 <jigoku_shujin@hotmail.com>
2017-07-12 22:40:07 -0500

    Add files via upload
    Crea un programa capaz de calcular la suma de los números que se indicarán en la entrada estándar, separados por espacios, y mostrar los resultados en pantalla. Los números pueden ser negativos, grandes y las líneas pueden contener espacios adicionales, por lo que el programa debe ser robusto.
    
    (Cuidado: no debe ser "amigable", sino estricto: no debe decir nada como "introduce los datos" ni responder con nada como "la suma es..."; debe tomar cada línea de datos de la entrada estándar, analizarla y mostrar los resultados en la salida estándar).

---
+++ Suma_Dificil.py
@@ -0,0 +1,39 @@
+"""
+Ejemplo de entrada
+123456789012 1
+2 3
+4   5 -1
+
+Ejemplo de salida
+123456789013
+5
+8
+"""
+
+contador = 0
+
+# Almacenara los resultados de las suma
+resultados = [] 
+
+while(contador <= 2):
+
+	numero_de_entradas = str(input())
+
+	# El metodo buscara y separara los numeros que tengan espacios
+	numeros = numero_de_entradas.split(" ")
+	suma = 0
+
+	# un for que descomprondra la lista y hara un recorrido
+	for x in numeros:
+		# Por si hay un caracter de doble espacio lo eliminamos
+		if(len(x) != 0):	
+			suma += int(x)   
+	
+	# Agregamos el resultado a unestra lista
+	resultados.append(suma)
+	contador += 1
+
+print("\n\n")
+
+for x in resultados:
+	print(x)
