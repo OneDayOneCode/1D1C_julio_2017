19b6897f402cd8361a325677565729630b18fc9b
Marcos-1997 <jigoku_shujin@hotmail.com>
2017-07-24 21:00:45 -0500

    Programa hecho para POO.
    Se le pide al alumno que lea el archivo numeros.dat y determine cuantos numeros son positivos y cuantos negativos.

---
+++ Archivos_Numeros/CNumerosPN.java
@@ -0,0 +1,78 @@
+
+import java.util.Scanner;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+// Cuantos Numeros Positivos y Negativos hay: CNumerosPN
+public class CNumerosPN
+{
+	/*
+		Pequeño programa que lee en un archivo de texto los numeros y despues
+		te dice la cantidad de numeros negativos y positivos.
+		
+		Este programa fue hecho en Programación Orientada a Objetos.
+	*/
+	
+	private DataInputStream archivo;
+	private int numerosP;
+	private int numerosN;
+		
+	public CNumerosPN() throws IOException
+	{
+		this.archivo = new DataInputStream( new FileInputStream("numeros.dat"));
+	}
+	
+	// Numero de Numeros Positivos y Negativos: NPN
+	
+	/*
+		Deprecated significa que sólo está ahí para que las aplicaciones antiguas funcionen, pero no se debería usar en ningún caso en aplicaciones nuevas, 		porque ya hay formas mejores o más estables de hacer lo mismo. De hecho, los métodos "deprecated" es más que probable que desaparezcan en próximas 			versiones.
+
+	*/
+	@Deprecated
+	public void numeroDeNPN()
+	{
+		String linea = null;
+		Scanner sc = null;
+		
+		try
+		{
+			while((linea = this.archivo.readLine()) != null)
+			{
+				sc = new Scanner(linea);
+			
+				while(sc.hasNext())
+				{
+					int numero  = Integer.valueOf(sc.next());
+				
+					if(numero > 0)
+					{
+						this.numerosP++;
+					}
+				
+					else
+					{
+						this.numerosN++;
+					}
+				
+				}
+			}
+		}
+		
+		catch(IOException e)
+		{
+			System.err.println("[Error]A ocurrido un error.");
+		}
+		
+		finally
+		{
+			sc.close();
+		}
+	}
+	
+	//Cuantos numeros positivos y negativos hay : cnpynhy
+	public void cnpynhy()
+	{
+		System.out.printf("Hay %d numeros positivos.\nHay %d numeros negativos.\n", this.numerosP, this.numerosN);
+	}
+}
---
+++ Archivos_Numeros/Principal.java
@@ -0,0 +1,13 @@
+
+import java.io.IOException;
+
+public class Principal
+{	
+	@Deprecated
+	public static void main(String[] args) throws IOException
+	{
+		CNumerosPN cn = new CNumerosPN();
+		cn.numeroDeNPN();
+		cn.cnpynhy();		
+	}
+}
