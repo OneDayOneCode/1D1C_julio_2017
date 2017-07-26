91b71bd80d0f544f51f8d75eaddc6bf40aaf8382
Marcos-1997 <jigoku_shujin@hotmail.com>
2017-07-26 17:57:01 -0500

    Update Calculadora.cpp

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -3,33 +3,33 @@ using namespace std;
 
 int main()
 {
-	int a;
-	int b;
-	char oper;
+    int a;
+    int b;
+    char oper;
 	
-	cout << "Digite la operacion a realizar sin espacios:\n\nOperacion:";
-	cin >> a >> oper >> b;
+     cout << "Digite la operacion a realizar sin espacios:\n\nOperacion:";
+     cin >> a >> oper >> b;
 	
-	switch(oper)
-	{
-		case '+':
-				 cout << a << "+" << b << "=" << (a + b) << endl;
-		break;
+     switch(oper)
+     {
+	case '+':
+		 cout << a << "+" << b << "=" << (a + b) << endl;
+	break;
 		
-		case '-':
-				cout << a << "-" << b << "=" << (a - b) << endl;
-		break;
+	case '-':
+		cout << a << "-" << b << "=" << (a - b) << endl;
+	break;
 	
-		case '*':
-				cout << a << "*" << b << "=" << (a * b) << endl;
-		break;
+	case '*':
+		cout << a << "*" << b << "=" << (a * b) << endl;
+	break;
 	
-		case '/':
-				cout << a << "/" << b << "=" << (a / b) << endl;
-		break;
+	case '/':
+		cout << a << "/" << b << "=" << (a / b) << endl;
+	break;
 	
-		default:
-				cout << "La operacion no existe" << endl;
-		break;
-	}
+	default:
+		cout << "La operacion no existe" << endl;
+	break;
+      }
 }
